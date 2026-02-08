3db454867a9e6ecf89ac0145948671fd21690ea9
Ben Powley <ben-powley@outlook.com>
2026-02-08 12:57:50 +0000

    fix: update BrowserRouter for future features

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <Routes>
         <Route path="/">
           <Route
